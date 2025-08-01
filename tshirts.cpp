72f567bf8104fb44453532e605041b4bea818540
vathsaaaa <m.srivathsa@in.bosch.com>
2025-08-01 09:59:42 +0530

    Update logic and Testcases

--- tshirts.cpp
+++ tshirts.cpp
@@ -3,9 +3,9 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms <= 38) {
         sizeName = 'S';
-    } else if(cms >= 38 && cms < 42) {
+    } else if(cms > 38 && cms < 42) {
         sizeName = 'M';
     } else if(cms >= 42) {
         sizeName = 'L';
@@ -15,10 +15,10 @@ char size(int cms) {
 
 void testTshirtSize() {
     printf("\nTshirt size test\n");
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    assert(size(38) == 'M');
-    assert(size(42) == 'L');
+    //assert(size(37) == 'S');
+    assert(size(40) == 'M'); //Any number 39, 40, 41 can be used. 
+    //assert(size(43) == 'L');
+    assert(size(38) == 'S'); //Edge Case 1
+    assert(size(42) == 'L'); //Edge Case 2
     printf("All is well (maybe!)\n");
 }
